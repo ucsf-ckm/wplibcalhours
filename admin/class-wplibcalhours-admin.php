4c393690a793a763e00d6b1b78467140a14dfeb8
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-04-09 21:47:11 -0700

    rm unused style and css admin hooks.

--- admin/class-wplibcalhours-admin.php
+++ admin/class-wplibcalhours-admin.php
@@ -55,60 +55,6 @@ class WpLibCalHours_Admin {
 	}
 
 	/**
-	 * Register the stylesheets for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in WpLibCalHours_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The WpLibCalHours_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->plugin_name,
-			plugin_dir_url( __FILE__ ) . 'css/wplibcalhours-admin.css',
-			array(),
-			$this->version,
-			'all' );
-
-	}
-
-	/**
-	 * Register the JavaScript for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in WpLibCalHours_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The WpLibCalHours_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name,
-			plugin_dir_url( __FILE__ ) . 'js/wplibcalhours-admin.js',
-			array( 'jquery' ),
-			$this->version,
-			false );
-
-	}
-
-	/**
 	 * Add an options page under the Settings submenu
 	 *
 	 * @since  1.0.0
