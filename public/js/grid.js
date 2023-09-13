50721eb604af6154435c642aadaaee8ebcd6a77b
lfarrell <lfarrell@email.unc.edu>
2023-09-13 11:53:49 -0400

    Fix issue with initial stack layout being switched to grid when hours are opened and then collapsed

--- public/js/grid.js
+++ public/js/grid.js
@@ -4,11 +4,12 @@
         if (clicked_hours_list == null) { // User didn't click hours button
             return;
         }
+        const has_grid_layout = hasInitialGridLayout(clicked_hours_list);
         const hours = clicked_hours_list.querySelector('.closed');
         const button_text = document.getElementById(e.target.id);
 
         if (hours !== null) {
-            setStacked(clicked_hours_list);
+            setStacked(clicked_hours_list, has_grid_layout);
 
             hours.classList.remove('closed');
             hours.classList.add('open');
@@ -21,13 +22,19 @@
             hide_hours.setAttribute('aria-hidden', 'true');
             button_text.textContent = 'View all hours';
 
-            setTimeout(setStacked, 1200, clicked_hours_list);
+            setTimeout(setStacked, 1200, clicked_hours_list, has_grid_layout);
         }
     }
 
-    function setStacked(clicked_hours_list) {
-        clicked_hours_list.classList.toggle('hours-display-stacked');
-        clicked_hours_list.classList.toggle('hours-display');
+    function setStacked(clicked_hours_list, initial_layout) {
+        if (initial_layout) {
+            clicked_hours_list.classList.toggle('hours-display-stacked');
+            clicked_hours_list.classList.toggle('hours-display');
+        }
+    }
+
+    function hasInitialGridLayout(clicked_hours_list) {
+        return clicked_hours_list.classList.contains('hours-display');
     }
 
     document.addEventListener('click', showHours);
